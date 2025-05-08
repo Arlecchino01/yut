9e9f5dd1bbaf38a2504e8a0e469445d547be4118
nZero <damun902@gmail.com>
2025-05-08 22:55:19 +0900

    ad

--- GameManager.java
+++ GameManager.java
@@ -1,3 +1,55 @@
+import java.util.*;
+
 public class GameManager {
+    int playerNum;
+    List<Player> players;
+
+
+    
+    
+    public GameManager(){
+        System.out.println("게임을 시작합니다.");
+        System.out.println("플레이어의 수를 입력하세요: ");
+        
+        Scanner sc = new Scanner(System.in);
+        playerNum = sc.nextInt();
+        sc.close();
+
+        for(int i = 0; i<playerNum; i++){
+
+
+            
+
+            Player player = createPlayer();
+
+            System.out.println("이름을 입력하세요: ");
+            Scanner sc1 = new Scanner(System.in);
+            String thisName = sc1.nextLine();
+            player.name = thisName;
+            System.out.println(thisName);
+            //players.add(player);
+            //String checkName = players.get(i).name;
+            //System.out.println("플레이어"+ (i+1) + checkName + "님이 추가되었습니다.");
+            sc1.close();
+        }
+
+
+
+        sc.close();
+    }
+
+    
+    
+    public Player createPlayer(){
+        Player p = new Player();
+        return p;
+    }
+
+
+
+    
+
+    
+
 
 }
--- Main.java
+++ Main.java
@@ -1,12 +1,6 @@
 
 public class Main {
     public static void main(String[] args) {
-        Board a = new Board(4);
-        Tile tileA;
-        tileA = a.edges.get(0).tiles.get(0);
-        System.out.println(tileA.id);
-        System.out.println();
-        Tile tileB = a.diagonalsFromCenter.get(0).tiles.get(0);
-        System.out.println(tileB.id);
+        GameManager a = new GameManager();
     }
 }
